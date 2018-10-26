9dfdc138808bdf9d6a872fcf3428d045dd8fe97b
Naung Soe <naungsoe@mceducation.com>
2018-10-26 14:55:02 +0800

    added drawer and updated default theme

--- mconline/routes/tests.js
+++ mconline/routes/tests.js
@@ -6,7 +6,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/create', function(req, res, next) {
-  res.render('tests/creat-test', { title: 'MC Education' });
+  res.render('tests/create-test-1', { title: 'MC Education' });
+});
+
+router.get('/create/2', function(req, res, next) {
+  res.render('tests/create-test-2', { title: 'MC Education' });
 });
 
 router.get('/track', function(req, res, next) {
