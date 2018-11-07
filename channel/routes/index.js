475b1fdcda25c8a491b7d2a113b7421ba5b7f8ee
Naung Soe <naungsoe@mceducation.com>
2018-11-07 11:56:18 +0800

    updated player structure and style

--- channel/routes/index.js
+++ channel/routes/index.js
@@ -3,11 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: ' Marshall Cavendish Education' });
+  res.render('index', {
+		layout: 'layout',
+		title: 'Marshall Cavendish Education'
+	});
 });
 
-router.get('/theme', function(req, res, next) {
-  res.render('theme', { title: ' Marshall Cavendish Education' });
+router.get('/topic', function(req, res, next) {
+  res.render('topic/index', {
+		layout: 'topic/player_layout',
+		title: 'Marshall Cavendish Education'
+	});
+});
+
+router.get('/topic/resource', function(req, res, next) {
+  res.render('topic/resource/index', {
+		layout: 'topic/resource/player_layout',
+		title: 'Marshall Cavendish Education'
+	});
 });
 
 module.exports = router;
