bdd9491dd40c9ded3372b1c3277598f4242b19dd
Naung Soe <naungsoe@mceducation.com>
2018-11-12 12:28:53 +0800

    added offline html files

--- channel/routes/index.js
+++ channel/routes/index.js
@@ -4,14 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-		layout: 'layout',
 		title: 'Marshall Cavendish Education'
 	});
 });
 
 router.get('/player', function(req, res, next) {
   res.render('player/index', {
-		layout: 'player/player_layout',
 		title: 'Marshall Cavendish Education'
 	});
 });
